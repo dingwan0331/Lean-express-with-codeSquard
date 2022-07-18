e0419ebd85a225e57b19fa4b4c49c48fe87e15b9
dingwan <dingwan0331@gmail.com>
2022-07-18 13:13:50 +0900

    CREATE : .env & .gitignore

--- router/email.js
+++ router/email.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router  = express.Router(); // express의 Router 메서드
 const path    = require('path');  // 상대경로를 사용하기위해 path 모듈 불러오기
 const mysql   = require('mysql')
+const dotenv  = require("dotenv");
 
-const connection = mysql.createConnection({ // mysql 과 연동할 기본 셋팅들을 인자로 넣어준다.
-    host     : 'localhost',       // mysql end-point 지금은 local 환경의 mysql을 사용하기에 localhost로 적었다.
-    port     : '3306',            // mysql default port number
-    user     : 'root',            // mysql 의 유네임을 넣어준다.
-    password : '',                // mysql 의 pw를 넣어줘야하지만 외부 모듈 + gitignore 관리를 배우기전까지 공백으로 유지할 예정이다.
-    database : 'express_example'  // 사용할 schema를 입력
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host     : process.env.MYSQL_HOST,
+    port     : process.env.MYSQL_PORT,   
+    user     : process.env.MYSQL_USERNAME,   
+    password : process.env.MYSQL_PASSWORD,
+    database : process.env.MYSQL_SCHEMA_NAME
   });
    
 router.get('', function(req, res){
